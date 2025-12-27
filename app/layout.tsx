dd66dacea7d8246ab6695ee75e3eb847ce63a471
Жуков Денис Владимирович <DZhukov4@alfabank.ru>
2025-12-28 00:41:00 +0300

    fix: add yandex verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -92,6 +92,7 @@ export default function RootLayout({
         <html lang="ru" suppressHydrationWarning>
         <head>
             <meta name="google-site-verification" content="XHWnek4KF6QdZm1UFnJeFW8xUtW1ZXbZkIHO84ZD5xs" />
+            <meta name="yandex-verification" content="91fc20500288b2fe" />
             <script
                 type="application/ld+json"
                 dangerouslySetInnerHTML={{
