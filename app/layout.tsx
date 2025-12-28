833c8071bd90d5b91c0f1424e083266a79a74228
Жуков Денис Владимирович <DZhukov4@alfabank.ru>
2025-12-28 22:14:03 +0300

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -123,6 +123,10 @@ export default function RootLayout({
     return (
         <html lang="ru" suppressHydrationWarning>
         <head>
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+            <link rel="manifest" href="/site.webmanifest"/>
             <meta name="google-site-verification" content="XHWnek4KF6QdZm1UFnJeFW8xUtW1ZXbZkIHO84ZD5xs"/>
             <meta name="yandex-verification" content="91fc20500288b2fe"/>
             <script
