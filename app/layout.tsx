c05842a31dad98571662e0cd85175d67811ec892
Жуков Денис Владимирович <DZhukov4@alfabank.ru>
2026-01-04 21:12:48 +0300

    feat(layout): add yandex counter

--- app/layout.tsx
+++ app/layout.tsx
@@ -203,6 +203,30 @@ export default function RootLayout({
                     }),
                 }}
             />
+
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106114023', 'ym');
+              ym(106114023, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+            `,
+                }}
+            />
+            <noscript>
+                <div>
+                    <img
+                        src="https://mc.yandex.ru/watch/106114023"
+                        style={{position: 'absolute', left: '-9999px'}}
+                        alt=""
+                    />
+                </div>
+            </noscript>
+
         </head>
 
         <body className={`${geistSans.variable} ${geistMono.variable}`}>
