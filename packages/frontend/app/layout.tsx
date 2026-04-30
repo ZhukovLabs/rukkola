cf416792679d87d50f1d949d5de4ed3e5435a521
Denis <deniszhukov.hachiko@gmail.com>
2026-05-01 01:37:48 +0300

    feat: remove yandex metrica

--- packages/frontend/app/layout.tsx
+++ packages/frontend/app/layout.tsx
@@ -1,6 +1,5 @@
 import type {Metadata, Viewport} from "next";
 import {Geist, Geist_Mono} from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 import {Providers} from "@/components/providers";
 
@@ -263,33 +262,6 @@ export default function RootLayout({
                     dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
                 />
             ))}
-
-            <Script
-                id="ym-tag"
-                src="https://mc.yandex.ru/metrika/tag.js?id=106114023"
-                strategy="lazyOnload"
-            />
-            <Script
-                id="ym-init"
-                strategy="lazyOnload"
-                dangerouslySetInnerHTML={{
-                    __html: `
-                        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                        m[i].l=1*new Date();
-                        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-                        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-                        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-                        ym(106114023, "init", {
-                                clickmap:true,
-                                trackLinks:true,
-                                accurateTrackBounce:true,
-                                webvisor:true,
-                                ecommerce:"dataLayer"
-                        });
-                    `,
-                }}
-            />
         </head>
 
         <body className={`${geistSans.variable} ${geistMono.variable}`}>
