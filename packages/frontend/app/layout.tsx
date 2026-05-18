9d865951c63a1bbaffdda35105fe97f371d8b6c7
Denis <deniszhukov.hachiko@gmail.com>
2026-05-18 14:27:01 +0300

    fix: change color theme

--- packages/frontend/app/layout.tsx
+++ packages/frontend/app/layout.tsx
@@ -212,8 +212,8 @@ export const viewport: Viewport = {
     initialScale: 1,
     maximumScale: 5,
     themeColor: [
-        {media: "(prefers-color-scheme: light)", color: "#ffffff"},
-        {media: "(prefers-color-scheme: dark)", color: "#ffffff"},
+        {media: "(prefers-color-scheme: light)", color: "#0a0a0a"},
+        {media: "(prefers-color-scheme: dark)", color: "#0a0a0a"},
     ],
 };
 
